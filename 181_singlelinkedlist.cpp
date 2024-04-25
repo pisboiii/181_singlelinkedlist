0771cdc657881ff08515561c9956424d97de9965
pisboiii <hfzfahrzi@gmail.com>
2024-04-24 20:41:10 -0700

    bool delete node (fungsi untuk menghapus data)

--- 181_singlelinkedlist.cpp
+++ 181_singlelinkedlist.cpp
@@ -67,7 +67,7 @@ bool searchNode(int nim, Node* current, Node* previous) {
     }
 }
 
-bool deleteNode(int nim) {
+bool deleteNode(int nim) { 
     Node* current = START;
     Node* previous = START;
     if (searchNode(nim, previous, current) ==
