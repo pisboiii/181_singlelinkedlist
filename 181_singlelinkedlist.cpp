2ffc480ceb70329856f17ce8306169b98d36fab0
pisboiii <hfzfahrzi@gmail.com>
2024-04-24 20:44:18 -0700

    menampilkan opsi menambah data

--- 181_singlelinkedlist.cpp
+++ 181_singlelinkedlist.cpp
@@ -132,7 +132,7 @@ int main()
     {
         try
         {
-            cout << "1. Tambah Data" << endl;
+            cout << "1. Tambah Data" << endl; 
             cout << "2. Hapus Data" << endl; cout << "3. Tampilkan Data" << endl;
             cout << "4. Cari Data" << endl;
             cout << "5. Keluar" << endl;
