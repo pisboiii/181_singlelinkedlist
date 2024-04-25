e249e37e87e94effcba20defbd5c426eb4ba4571
pisboiii <hfzfahrzi@gmail.com>
2024-04-24 20:45:13 -0700

    menampilkan opsi untuk keluar

--- 181_singlelinkedlist.cpp
+++ 181_singlelinkedlist.cpp
@@ -135,7 +135,7 @@ int main()
             cout << "1. Tambah Data" << endl; 
             cout << "2. Hapus Data" << endl; cout << "3. Tampilkan Data" << endl; 
             cout << "4. Cari Data" << endl; 
-            cout << "5. Keluar" << endl;
+            cout << "5. Keluar" << endl; 
             cout << "Pilihan: ";
             cin >> pilihan;
             switch (pilihan)
